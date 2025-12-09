308109f0f406e53982361ebb657ed28e95a760ea
Matheusmnttiii <matheus.chamorro@estudante.ifms.edu.br>
2025-12-09 19:03:20 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
 export async function getServerSideProps() {
   // Fetch data from  API notícias
-  const res = await fetch('https://noticiasapi-omega.vercel.app/noticias')
+  const res = await fetch('https://apimatheus.vercel.app/')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
